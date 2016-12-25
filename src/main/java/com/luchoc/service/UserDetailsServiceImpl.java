f79e8b16e81c32183c48f2d2571b6cd07952f706
luchoc <luiscarlin@gmail.com>
2016-12-24 21:11:54 -0500

    Throw exception when user not found and add logging

--- src/main/java/com/luchoc/service/UserDetailsServiceImpl.java
+++ src/main/java/com/luchoc/service/UserDetailsServiceImpl.java
@@ -3,6 +3,8 @@ package com.luchoc.service;
 import com.luchoc.model.Role;
 import com.luchoc.model.User;
 import com.luchoc.repository.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -16,14 +18,26 @@ import java.util.HashSet;
 import java.util.Set;
 
 @Service
-public class UserDetailsServiceImpl implements UserDetailsService{
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(UserDetailsServiceImpl.class);
+
     @Autowired
     private UserRepository userRepository;
 
     @Override
     @Transactional(readOnly = true)
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        LOGGER.debug("Authenticating user with username={}", username);
+
         User user = userRepository.findByUsername(username);
+
+        if (user == null) {
+            LOGGER.debug("User with username={} was not found", username);
+            throw new UsernameNotFoundException(String.format("User with username=%s was not found", username));
+        }
+
+        LOGGER.debug("Building a new userdetails User for user with username={}", username);
 
         Set<GrantedAuthority> grantedAuthorities = new HashSet<>();
         for (Role role : user.getRoles()){
